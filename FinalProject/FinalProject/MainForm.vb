919fbce7bee49ea5479b44d847bb3d2b51f204ed
LAPTOP-973K5CDJ\Sam Stuber <samstuber1997@gmail.com>
2017-11-15 18:56:46 -0500

    I created a database and two tables for my final project

---
+++ FinalProject/FinalProject/MainForm.vb
@@ -0,0 +1,34 @@
+﻿Option Strict On
+Option Explicit On
+
+Public Class MainForm
+    Private Sub Label1_Click(sender As Object, e As EventArgs) 
+
+    End Sub
+
+    Private Sub CloseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CloseToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub AddVehicleToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AddVehicleToolStripMenuItem.Click
+        FrmAddVehicle.ShowDialog()
+    End Sub
+
+    Private Sub RemoveVehicleToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RemoveVehicleToolStripMenuItem.Click
+        FrmRemoveVehicle.ShowDialog()
+    End Sub
+
+    Private Sub ViewInventoryToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ViewInventoryToolStripMenuItem.Click
+        FrmInventory.ShowDialog()
+
+    End Sub
+
+
+    Private Sub InventoryStatusToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles InventoryStatusToolStripMenuItem.Click
+        FrmDealerStatus.ShowDialog()
+    End Sub
+
+    Private Sub SoldVehiclesToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SoldVehiclesToolStripMenuItem.Click
+        FrmSoldVehicles.ShowDialog()
+    End Sub
+End Class
