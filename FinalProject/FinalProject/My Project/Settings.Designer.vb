7edf81f20a8160f05754969c173c6adb1064df18
LAPTOP-973K5CDJ\Sam Stuber <samstuber1997@gmail.com>
2017-11-18 11:52:53 -0500

    I created 2 sequel functions: one to get inventory cost and one to get  profit.

--- FinalProject/FinalProject/My Project/Settings.Designer.vb
+++ FinalProject/FinalProject/My Project/Settings.Designer.vb
@@ -57,8 +57,8 @@ Namespace My
         <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
          Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""|DataDirectory|\Stuber'sAuto"& _ 
-            "Sales.mdf"";Integrated Security=True")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DreamVehicles"& _ 
+            ".mdf;Integrated Security=True")>  _
         Public ReadOnly Property DreamVehiclesConnectionString() As String
             Get
                 Return CType(Me("DreamVehiclesConnectionString"),String)
